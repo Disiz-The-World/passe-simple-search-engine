5932ab38fbd67ce573a32597b7a97379c0e1b3cf
Thomas <thomas@socraft.ch>
2025-03-25 23:13:57 +0100

    deps: add prettier and eslint

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    MatSidenavContent,
+    MatToolbar,
+    MatIcon,
+    MatNavList,
+    MatSidenav,
+    MatSidenavContainer,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
