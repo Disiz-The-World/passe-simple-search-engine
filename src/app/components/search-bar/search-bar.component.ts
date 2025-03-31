ec7b3d59a8b67cb9d70fe1819782ce9e95f64e24
Thomas <thomas@socraft.ch>
2025-03-31 17:43:51 +0200

    style: add border-radius search-bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
-import { MatAutocomplete, MatOption } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,11 +12,11 @@ import { MatAutocomplete, MatOption } from '@angular/material/autocomplete';
     MatFormFieldModule,
     MatIconModule,
     CommonModule,
-    MatAutocomplete,
     FormsModule,
     MatInput,
   ],
   styleUrls: ['./search-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SearchBarComponent {
   searchQuery: string | undefined;
