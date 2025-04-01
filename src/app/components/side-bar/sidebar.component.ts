3bf0ff4d5d3aa046ef06dd32e352aa3bfc123fc3
Thomas <thomas@socraft.ch>
2025-04-01 13:45:40 +0200

    style: remake style for sidebar

--- src/app/components/side-bar/sidebar.component.ts
+++ src/app/components/side-bar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -9,6 +9,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [CommonModule, MatIcon, RouterLink, RouterLinkActive],
   styleUrls: ['./sidebar.component.scss'],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SidebarComponent {
   navItems = [
@@ -20,6 +21,6 @@ export class SidebarComponent {
 
   bottomItems = [
     { name: 'Mon compte', icon: 'account_circle', route: '/account' },
-    { name: 'Informations légales', icon: 'legal', route: '/legal' },
+    { name: 'Informations légales', icon: 'description', route: '/legal' },
   ];
 }
