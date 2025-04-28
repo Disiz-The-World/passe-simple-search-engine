49d68809cdf1214ece2de96e0451915b8dfd1b87
link1183 <adrien.gunther@gmail.com>
2025-04-28 13:10:16 +0200

    Error page start

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
