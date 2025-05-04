8d0648e106be44d5f9950524f8b24b0b53dcb3bd
loic metrailler <loic@metrailler.me>
2025-05-04 21:48:52 +0200

    [ADD] I added a few componants for the detail page

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { DetailViewComponent } from '../components/detail-view/detail-view.component';
 
 export const routes: Routes = [
-  { path: 'balades/:id', component: DetailViewComponent }
+  { path: 'balades/:id', component: DetailViewComponent },
 ];
