c2d44765b0742a2afea8b5646ef7cd7568194375
Thomas <thomas@socraft.ch>
2025-05-06 23:49:02 +0200

    feat: add login in avatar

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { NgOptimizedImage } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+
 @Component({
   selector: 'app-avatar',
+  standalone: true,
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
-  standalone: true,
-  imports: [MatIconModule, MatCardModule, NgOptimizedImage],
+  imports: [MatIconModule, MatCardModule, NgOptimizedImage, MatDialogModule]
 })
 export class AvatarComponent {
-  avatar: string = 'avatar.jpeg';
+  avatar: string = 'assets/avatar.jpeg';
+
+  constructor(private dialog: MatDialog) {}
+
+  openLoginDialog() {
+    this.dialog.open(LoginComponent, {
+      width: '400px',
+      panelClass: 'login-dialog-panel',
+    });
+  }
 }
