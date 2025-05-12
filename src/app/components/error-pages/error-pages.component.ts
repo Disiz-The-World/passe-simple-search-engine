21b71694b26d9bad6fbcbe31d632c4373ef122a5
link1183 <adrien.gunther@gmail.com>
2025-05-12 09:22:22 +0200

    Finished the 404 page

--- src/app/components/error-pages/error-pages.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-error-pages',
-  imports: [],
-  templateUrl: './error-pages.component.html',
-  styleUrl: './error-pages.component.scss',
-})
-export class ErrorPagesComponent {}
