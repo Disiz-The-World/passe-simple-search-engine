826a5c5433af15fe82d80226aab0398615c1b10b
link1183 <adrien.gunther@gmail.com>
2025-05-15 20:15:42 +0200

    Npm install with the script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-(cd ./passe-simple-search-engine && ng serve) &
-(cd ./backend && npx json-server db.json --static public) &
+(cd ./passe-simple-search-engine && npm install && ng serve) &
+(cd ./backend && npm install && npx json-server db.json --static public) &
 wait
