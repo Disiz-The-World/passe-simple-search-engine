b2b3dd552bfcd31ba8fc6f4733a8a85446606f21
link1183 <adrien.gunther@gmail.com>
2025-05-16 11:54:27 +0200

    Fuck windows
    Windows is apparently unable to spawn a shell in the current dir from a
    .bat file. I fucking hate windows with all my soul

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
 @echo off
-start cmd /k "cd passe-simple-search-engine && npm install && ng serve"
-start cmd /k "cd backend && npm install && npx json-server db.json --static public"
+setlocal
+
+set ROOT=%~dp0
+
+start cmd /k "cd /d %ROOT%passe-simple-search-engine && npm install && ng serve"
+start cmd /k "cd /d %ROOT%backend && npm install && npx json-server db.json --static public"
 
